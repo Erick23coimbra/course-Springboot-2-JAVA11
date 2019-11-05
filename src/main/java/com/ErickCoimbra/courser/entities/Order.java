8b29b26e2a4208e3f9b96667e49d9e940ec700fb
Erick Coimbra <erick23Coimbra@gmail.com>
2019-11-04 21:11:00 -0300

    master

--- src/main/java/com/ErickCoimbra/courser/entities/Order.java
+++ src/main/java/com/ErickCoimbra/courser/entities/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.ErickCoimbra.courser.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
@@ -23,7 +24,8 @@ public class Order implements Serializable {
 	private Long id;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-
+	
+	private Integer orderStatus;
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -32,8 +34,9 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		super();
+		setOrderStatus(orderStatus);
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
@@ -53,6 +56,19 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+	
+	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus) ;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();	
+		}
+		
 	}
 
 	public User getClient() {
