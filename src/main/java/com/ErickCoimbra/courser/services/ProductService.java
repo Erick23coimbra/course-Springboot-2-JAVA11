edb6964e697cb6cbba2a494071e7b44ab5a649d2
Erick Coimbra <erick23Coimbra@gmail.com>
2019-11-06 23:05:21 -0300

    Product

---
+++ src/main/java/com/ErickCoimbra/courser/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.ErickCoimbra.courser.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ErickCoimbra.courser.entities.Product;
+import com.ErickCoimbra.courser.repositories.ProductRepository;
+@Service
+public class ProductService {
+	@Autowired // ingeção de dependencia transparente
+	private ProductRepository  repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	public Product findById(Long id) {
+		Optional <Product> obj =repository.findById(id);
+		return obj.get();
+	}
+
+}
